dd6fb32b6c0d0ee96f3aa33e0e1976d64dae2d93
nongbaoting <523135753@qq.com>
2022-07-08 03:00:05 +0800

    routine backup 20220708

--- src/views/crispr/browse.vue
+++ src/views/crispr/browse.vue
@@ -1,20 +1,114 @@
 <template>
-  <div>
-    <h1>crispr</h1>
+  <div class="container">
     <el-row>
-      <el-table id="tabCrispr">
-        <el-table-column label="Accession"></el-table-column>
-        <el-table-column label="Name"></el-table-column>
-        <el-table-column label="Specie"></el-table-column>
-        <el-table-column label="Length"></el-table-column>
-        <el-table-column label="SP-Score"></el-table-column>
+      <el-table
+        :key="'tabCrispr' + currentPage + pageSize"
+        ref="tabCrispr"
+        :data="tableData"
+        v-loading="loading"
+        element-loading-text="loading"
+        element-loading-spinner="el-icon-loading"
+        element-loading-background="rgba(0, 0, 0, 0.8)"
+      >
+        <el-table-column label="Accession" prop="accession"></el-table-column>
+        <el-table-column label="CAS" prop="cas_class"></el-table-column>
+        <el-table-column
+          label="Data Class"
+          prop="/prdata_class"
+        ></el-table-column>
+
+        <el-table-column label="Entry name" prop="entry_name"></el-table-column>
+        <el-table-column label="Gene Name" prop="gene_name"></el-table-column>
+        <el-table-column
+          label="Organism"
+          prop="organism"
+          width="320"
+        ></el-table-column>
+        <el-table-column
+          label="Protein Name"
+          prop="protein_name"
+        ></el-table-column>
+
+        <el-table-column
+          label="Length"
+          prop="sequence_length"
+        ></el-table-column>
+        <el-table-column label="Structure"></el-table-column>
       </el-table>
+    </el-row>
+
+    <!--分页条 -->
+    <el-row :gutter="40" justify="end" style="padding: 10px">
+      <!-- 分页 -->
+      <el-col>
+        <div class="pagination">
+          <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="currentPage"
+            :page-sizes="[5, 10, 20, 50, 100]"
+            :page-size="pageSize"
+            layout="total, sizes, prev, pager, jumper, next"
+            :total="totalCount"
+            prev-text="Prev"
+            next-text="Next"
+            background
+          >
+          </el-pagination>
+        </div>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      tableData: [],
+      currentPage: 1, //默认显示页面为1
+      pageSize: 10, //    每页的数据条数
+      loading: false,
+      totalCount: 0,
+    }
+  },
+  mounted() {
+    this.getData(10, 1)
+  },
+  methods: {
+    getData(pageSize, currentPage) {
+      this.loading = true
+      this.$http
+        .get('crispr/api/browse', {
+          params: {
+            pageSize: pageSize,
+            currentPage: currentPage,
+            // order: order,
+            // columnName: columnName,
+          },
+        })
+        .then((response) => {
+          console.log(response.data)
+          this.tableData = response.data.data
+          this.totalCount = response.data.totalCount
+          console.log(this.tableData)
+          this.loading = false
+        })
+    },
+
+    //每页下拉显示数据
+    handleSizeChange: function (size) {
+      this.pageSize = size
+      this.currentPage = 1
+      this.getData(this.pageSize, this.currentPage)
+    },
+    //点击第几页
+    handleCurrentChange: function (currentPage) {
+      this.currentPage = currentPage
+      this.getData(this.pageSize, this.currentPage)
+    },
+  },
+}
 </script>
 
 <style>
---
+++ src/views/crispr/common/Molstar.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div id="molstar_large"></div>
+  </div>
+</template>
+
+<script>
+import { Viewer } from 'molstar/build/viewer/molstar'
+import 'molstar/build/viewer/molstar.css'
+export default {
+  name: 'Molstar',
+
+  data() {
+    return {
+      pdb: '',
+    }
+  },
+  mounted() {
+    this.getPDB()
+  },
+
+  methods: {
+    getPDB() {
+      this.$http({
+        url: '/crispr/api/structure/getFile/',
+        params: {
+          filetype: 'file',
+          filename: this.$route.query.filename,
+        },
+        method: 'GET',
+      }).then((response) => {
+        console.log(response)
+        const url = window.URL.createObjectURL(new Blob([response.data]))
+        this.pdb = url
+        this.molstar()
+      })
+    },
+
+    molstar() {
+      this.$nextTick(function () {
+        // Code that will run only after the
+        // entire view has been re-rendered
+        let viewer = new Viewer('molstar_large', {
+          layoutIsExpanded: false,
+          layoutShowControls: true,
+          layoutShowRemoteState: false,
+          layoutShowSequence: true,
+          layoutShowLog: false,
+          layoutShowLeftPanel: true,
+
+          viewportShowExpand: true,
+          viewportShowSelectionMode: true,
+          viewportShowAnimation: true,
+          // 'http://222.200.186.47/outputs/testPDB/AF-P04637-F1-model_v1.pdb.gz',
+        })
+        // viewer.loadPdb('7bv2')
+        // viewer.loadEmdb('EMD-30210', { detail: 6 })
+
+        viewer.loadStructureFromUrl(this.pdb, 'pdb')
+        console.log(this.pdb)
+      })
+    },
+  },
+}
+</script>
+
+<style  scoped>
+#molstar_large {
+  width: 100%;
+  height: 100%;
+}
+</style>
